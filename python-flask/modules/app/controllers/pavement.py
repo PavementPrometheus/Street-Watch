63a3a5b32b16ff542533cd4bc5fb8f3d2768dab1
Oreko <Oreko@users.noreply.github.com>
2019-02-05 20:14:34 -0800

    More pep8 changes, fixed bugs in reponses, moving to better errors.

--- python-flask/modules/app/controllers/pavement.py
+++ python-flask/modules/app/controllers/pavement.py
@@ -16,51 +16,56 @@ def create_data():
     """
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
-        data   = request.get_json()
+        data = request.get_json()
         if(data is not None):
             record = mongo.db.pavement.insert(data)
             if isinstance(record, list):
                 response = 'Inserted {} documents'.format(len(record))
-                result = {'message': response, 'ids': [str(ID) for ID in record],
-                 'hrefs': ["/pavement/" + str(ID) for ID in record]}
+                result = {'message': response,
+                          'ids': [str(ID) for ID in record],
+                          'hrefs': ["/pavement/" + str(ID) for ID in record]}
             else:
                 response = 'Inserted 1 document'
-                result = {'message': response, 'id': str(record),
-                 'href': "/pavement/" + str(ID)}
-            code   = 201
-    except:
+                result = {'message': response,
+                          'id': str(record),
+                          'href': "/pavement/" + str(record)}
+            code = 201
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
+
 
 @app.route('/pavement', methods=['GET'])
 def retrieve_data():
     """
     Function to query the database
     """
-    # TODO: Change this to format responses like multiple retrieve_document calls
-        # Will return a 207
+    # TODO: Change this to format responses like
+    # multiple retrieve_document calls
+    # Will return a 207
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
-        query     = request.args
+        query = request.args
         documents = mongo.db.pavement.find(query)
         if documents.count() > 0:
             result = {'results': list(documents)}
-            code   = 200
+            code = 200
         else:
             # If the results from the query is empty
             result = {'error': 'Resources not found'}
-            code   = 404
-    except:
+            code = 404
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
+
 
 @app.route('/pavement/<_id>', methods=['GET'])
 def retrieve_document(_id):
@@ -69,23 +74,24 @@ def retrieve_document(_id):
     """
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
         # We throw a 500 error if ObjectId doesn't parse the input correctly
         document = mongo.db.pavement.find({"_id": ObjectId(_id)})
         if document.count() > 0:
             result = {'result': document[0]}
-            code   = 200
+            code = 200
         else:
             # If the results from the query is empty
             response = 'Resource {} not found'.format(_id)
-            result   = {'error': response}
-            code     = 404
-    except:
+            result = {'error': response}
+            code = 404
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
+
 
 @app.route('/pavement', methods=['PATCH'])
 def update_data():
@@ -95,9 +101,10 @@ def update_data():
     # Will return a 207
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     # TODO
     return jsonify(result), code
+
 
 @app.route('/pavement/<_id>', methods=['PATCH'])
 def update_document(_id):
@@ -106,25 +113,29 @@ def update_document(_id):
     """
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
         data = request.get_json()
         if(data is not None):
-            # We throw a 500 error if ObjectId doesn't parse the input correctly
-            updated = mongo.db.pavement.update_one({"_id": ObjectId(_id)}, data)
+            # We throw a 500 error if ObjectId
+            # doesn't parse the input correctly
+            updated = mongo.db.pavement.update_one({"_id": ObjectId(_id)},
+                                                   data)
             if updated.modified_count > 0:
-                result = {'id': str(record), 'href': "/pavement/" + str(ID)}
-                code     = 200
+                result = {'id': str(_id),
+                          'href': "/pavement/" + str(_id)}
+                code = 200
             else:
                 # If the results from the query is empty
                 response = 'Resource {} not found'.format(_id)
-                result   = {'error': response}
-                code     = 404
-    except:
+                result = {'error': response}
+                code = 404
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
+
 
 @app.route('/pavement', methods=['DELETE'])
 def delete_data():
@@ -132,27 +143,28 @@ def delete_data():
     Function to remove documents from the database
     """
     # TODO: Change this to format responses like multiple delete_document calls
-        # Will return a 207
+    # Will return a 207
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
-        query    = request.get_json()
-        deleted  = mongo.db.pavement.delete_many(query)
-        count    = deleted.deleted_count
+        query = request.get_json()
+        deleted = mongo.db.pavement.delete_many(query)
+        count = deleted.deleted_count
         if count > 0:
             response = 'Deleted {} result(s)'.format(count)
-            result   = {'message': response}
-            code     = 200
+            result = {'message': response}
+            code = 200
         else:
             # If the results from the query is empty
             result = {'error': 'Not found'}
-            code   = 404
-    except:
+            code = 404
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
+
 
 @app.route('/pavement/<_id>', methods=['DELETE'])
 def delete_document(_id):
@@ -161,21 +173,21 @@ def delete_document(_id):
     """
     # Default return values
     result = {'error': 'Bad Request'}
-    code   = 400
+    code = 400
     try:
         # We throw a 500 error if ObjectId doesn't parse the input correctly
-        deleted  = mongo.db.pavement.delete_one({"_id": ObjectId(_id)})
+        deleted = mongo.db.pavement.delete_one({"_id": ObjectId(_id)})
         if deleted.deleted_count > 0:
             response = 'Deleted _id: {}'.format(_id)
-            result   = {'message': response}
-            code     = 200
+            result = {'message': response}
+            code = 200
         else:
             # If the results from the query is empty
             response = 'Resource {} not found'.format(_id)
-            result   = {'error': response}
-            code     = 404
-    except:
+            result = {'error': response}
+            code = 404
+    except Exception as inst:  # TODO Sometimes catches 400 errors.
         # Error while handling user request
-        result = {'error': 'Server Error'}
-        code   = 500
+        result = {'error': 'Server Error ' + str(type(inst)) + ' ' + str(inst)}
+        code = 500
     return jsonify(result), code
