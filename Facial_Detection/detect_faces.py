4033e7a437dd827a6b37b3a0262c2e5ba0f84d68
hugheste <hugheste@oregonstate.edu>
2019-02-19 00:45:18 -0800

    Added comments and clarifications

--- Facial_Detection/detect_faces.py
+++ Facial_Detection/detect_faces.py
@@ -1,9 +1,19 @@
-# import the necessary packages
+#*****************************************************
+# Certain sections of code are from following an online 
+# tutorial on OpenCV
+# Title: OpenCV Face Recognition
+# Author: Adrian Rosebrock
+# Date: Sept. 24th, 2018
+# Code version: 1.0
+# Availability: https://www.pyimagesearch.com
+#******************************************************
+
+# Import packages for program
 import numpy as np
 import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
+# Arguments for command line
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
@@ -17,46 +27,39 @@ ap.add_argument("-f", "--file",
 	help="filename of file containing images to test")
 args = vars(ap.parse_args())
 
-# load our serialized model from disk
+# Load serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# load the input image and construct an input blob for the image
-# by resizing to a fixed 300x300 pixels and then normalizing it
+# Create blob
 image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
 	(300, 300), (104.0, 177.0, 123.0))
 
-# pass the blob through the network and obtain the detections and
-# predictions
+# Generate detections and predictions
 print("[INFO] computing object detections...")
 net.setInput(blob)
 detections = net.forward()
 
-# loop over the detections
+# Loop through each detections
 for i in range(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with the
-	# prediction
 	confidence = detections[0, 0, i, 2]
 
-	# filter out weak detections by ensuring the `confidence` is
-	# greater than the minimum confidence
 	if confidence > args["confidence"]:
-		# compute the (x, y)-coordinates of the bounding box for the
-		# object
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		# Generate dimensions for box on face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
 
-		# draw the bounding box of the face along with the associated
-		# probability
+                # Create recangle around faces
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
 		cv2.rectangle(image, (startX, startY), (endX, endY),
 			(0, 0, 255), 2)
+                # Put rectangle on face
 		cv2.putText(image, text, (startX, y),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-# show the output image
+# Output the facial detection image
 cv2.imshow("Output", image)
 cv2.waitKey(0)
