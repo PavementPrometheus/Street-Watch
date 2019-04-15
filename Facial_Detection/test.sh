694e966f6eb27bc28a7225c5507e39b9111693b9
hugheste <hugheste@oregonstate.edu>
2019-04-15 03:02:24 -0700

    Added case to handle undetected faces

--- Facial_Detection/test.sh
+++ Facial_Detection/test.sh
@@ -11,3 +11,4 @@ python blur_faces.py --image michael_scott.jpg --prototxt deploy.prototxt --mode
 python blur_faces.py --image seniors.jpg --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
 python blur_faces.py --image texting.jpg --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
 python blur_faces.py --image hipsters.jpg --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
+python blur_faces.py --image test_bad_image.png --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
